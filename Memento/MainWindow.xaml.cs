ed5f50710ec4be00287422b9f090a6ad6950c23c
yippee-ki-yay <nesa993@gmail.com>
2015-08-03 22:19:28 +0200

    Initial code, basic gui layout and messing around with parsing

---
+++ Memento/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Memento
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        QuestionManager qm;
+        Tuple<string, string> curr;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            qm = new QuestionManager();
+            qm.loadFile("wut");
+            curr = qm.getRandom();
+            pitanjeText.Text = curr.Item1;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            curr = qm.getRandom();
+            pitanjeText.Text = curr.Item1;
+            odogovorText.Text = "";
+           
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            odogovorText.Text = curr.Item2;
+        }
+    }
+}
---
+++ Memento/QuestionManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    class QuestionManager
+    {
+        List<Tuple<string, string>> qaList { get; set; }
+        private Random rnd = new Random();
+ 
+        public QuestionManager()
+        {
+            qaList = new List<Tuple<string, string>>();
+        }
+
+        public bool loadFile(string filePath)
+        {
+            string text = System.IO.File.ReadAllText("sims_teorija.txt", Encoding.UTF8);
+
+            string[] parts = Regex.Split(text, @"\d.");
+                  
+
+            //string[] parts = results.Split('?');
+
+            for (int i = 1; i < parts.Length; ++i )
+            {
+                string[] t = parts[i].Split('?');
+
+                if(t.Length == 2)
+                    qaList.Add(new Tuple<string, string>(t[0], t[1]));
+            }
+
+            return true;
+        }
+
+        public Tuple<string, string> getRandom()
+        {
+            return qaList[rnd.Next(qaList.Count)];
+        }
+    }
+}
